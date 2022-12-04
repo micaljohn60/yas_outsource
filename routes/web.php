cc1c9b1e9091821bfb9e9e64e44497d3a376498e
ThuHtetDev <thuhtettun.dev@gmail.com>
2022-12-04 13:46:40 +0630

    fix: conflict_resolve

--- routes/web.php
+++ routes/web.php
@@ -29,21 +29,12 @@ Route::group(['middleware' => 'seller', 'prefix' => 'bizs'], function () {
     Route::get('/', [BizController::class, 'index'])->name('biz.index');
 });
 
-<<<<<<< HEAD
-Route::group(['middleware' => 'buyer'] , function () {
-
-    Route::group(['prefix' => 'proposals'] , function () {
-        Route::get('/create', [\App\Http\Controllers\ProposalController::class, 'create'])->name('proposal.create');
-        Route::post('/create', [\App\Http\Controllers\ProposalController::class, 'store'])->name('proposal.store');
-        Route::get('/', [\App\Http\Controllers\ProposalController::class, 'index'])->name('proposal.index');
-        Route::get('/{proposal}', [\App\Http\Controllers\ProposalController::class, 'show'])->name('proposal.show');
-=======
 Route::group(['middleware' => 'buyer'], function () {
     Route::group(['prefix' => 'proposals'], function () {
+        Route::get('/create', [ProposalController::class, 'create'])->name('proposal.create');
         Route::post('/create', [ProposalController::class, 'store'])->name('proposal.store');
         Route::get('/', [ProposalController::class, 'index'])->name('proposal.index');
         Route::get('/{proposal}', [ProposalController::class, 'show'])->name('proposal.show');
->>>>>>> cad94d32487a68af826e93399f17f052b93c831f
 
         Route::post('/upload-to-biz', [ProposalController::class, 'uploadProposalToBiz'])->name('proposal.biz.upload');
     });
